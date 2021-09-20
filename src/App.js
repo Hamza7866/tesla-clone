1358ec11481c4d5b6ed123650edd4187464e9c74
Hamza Muhammad <hamzamuhammad@Hamzas-Air.fios-router.home>
2021-09-20 03:58:15 -0400

    menu

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Alert />
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,73 @@
 import React from "react";
-
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="logo" />
+      </a>
+      <Menu>
+        <a href="">Model S</a>
+
+        <a href="">Model 3</a>
+
+        <a href="">Model X</a>
+
+        <a href="">Model Y</a>
+        <a href="">Solar Roof</a>
+        <a href="">Solar Panels</a>
+      </Menu>
+      <RightMenu>
+        <a href="">Shop</a>
+        <a href="">TESLA ACCOUNT</a>
+        <CustomIcon>
+          <MenuIcon />
+        </CustomIcon>
+      </RightMenu>
+    </Container>
   );
 };
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 6vh;
+  position: fixed;
+  align-items: center;
+  padding: 15px 20px;
+  cursor: pointer;
+  display: flex;
+  /* justify-content: space-around; */
+  align-items: center;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 200px;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 70px;
+  a {
+    font-weight: 600;
+    padding-left: 15px;
+    text-transform: uppercase;
+  }
+`;
+
+const CustomIcon = styled(MenuIcon)`
+  cursor: pointer;
+`;
